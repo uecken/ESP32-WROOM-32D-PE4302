8dafacc449c62246e9d1adbbbd4c5c87c9e1a856
kentugi <the.full.of.moon@gmail.com>
2020-04-08 01:16:03 +0900

    Update ESP32-WROOM-32D-PE4302.ino
    How to serial control
    ho,4,999 #Handover 999times at interval 4 seconds.
    tp,30,1 # For throughput. 4dB decreases every 30 seconds.
    set,12 # set 12dB manually.

--- ESP32-WROOM-32D-PE4302.ino
+++ ESP32-WROOM-32D-PE4302.ino
@@ -27,12 +27,12 @@ int j = 0;
 int tpi = 0;
 int hoi = 0;
 String msgs[6] = {"\0"};
-int interval,iteration;
+int interval,iteration,att;
 
 void setup() {
     Serial.begin(115200);
 
-//Pin Set to Digital
+//Pin Set to Digital GPIO
      pinMode(18, OUTPUT);
      pinMode(19, OUTPUT);
      pinMode(23, OUTPUT);
@@ -72,9 +72,10 @@ void loop() {
       interval = msgs[1].toInt()*1000;
       iteration = msgs[2].toInt();
       for(j=0;j<iteration;j++){
-        HandOver4dBstep(interval,"p18-12_and_p26-p14");
+        HandOver4dBstep(interval,"ho_p18-12_and_p26-p14");
 
         udp.beginPacket(udpAddress,udpPort);
+        udpLocalTime();
         udp.printf("Handover Iteration: %d", tpi++);
         udp.endPacket();
         Serial.println("UDP:Handover Iteration:" + String(tpi));
@@ -84,16 +85,32 @@ void loop() {
       //Input example: tp,30[s],1
       interval = msgs[1].toInt()*1000;
       //iteration = msgs[2].toInt();
-      Throughput4dBstep(interval,"p18-12_and_p26-p14");
+      Throughput4dBstep(interval,"tp_p18-12_and_p26-p14");
 
       udp.beginPacket(udpAddress,udpPort);
-      udp.printf("Throughputr Iteration: %d", hoi++);
+      udpLocalTime();
+      udp.printf("Throughputr Iteration: %d", tpi++);
       udp.endPacket();
-      Serial.println("UDP:Throughput Iteration:" + String(i));
+      printLocalTime();
+      Serial.println("UDP:Throughput Iteration:" + String(tpi));
+    }
+    else if(msgs[0] == "set" || msgs[0] == "setatt"){
+      //Input example:set,28
+      att = msgs[1].toInt();
+      setxdB(att,"normal");
+
+      udp.beginPacket(udpAddress,udpPort);
+      udpLocalTime();
+      udp.printf("Set %d dB.", att);
+      udp.endPacket();
+      printLocalTime();
+      Serial.println("Set " + String(att) + " dB");
     }
   }
-  
+
+  printLocalTime();
   udp.beginPacket(udpAddress,udpPort);
+  udpLocalTime();
   udp.printf("No: %d", i++);
   udp.endPacket();
   Serial.println("No: " + String(i));
@@ -118,7 +135,24 @@ void setPin(String pinArrange, unsigned char v1_HL,unsigned char v2_HL,unsigned 
     digitalWrite(27,v5_HL); // V5 8dB
     digitalWrite(14,v6_HL); // V6 16dB
   }
-  else if(pinArrange =="p18-12_and_p26-p14"){ 
+  else if(pinArrange =="ho_p18-12_and_p26-p14"){ 
+    //for handover
+    digitalWrite(18,v1_HL); // V1 0.5dB
+    digitalWrite(19,v2_HL); // V2 1dB
+    digitalWrite(23,v3_HL); // V3 2dB 
+    digitalWrite(5,v4_HL); // V4 4dB
+    digitalWrite(13,v5_HL); // V5 8dB
+    digitalWrite(12,v6_HL); // V6 16dB
+    
+    digitalWrite(26,1-v1_HL); // V1 0.5dB
+    digitalWrite(25,1-v2_HL); // V2 1dB
+    digitalWrite(17,1-v3_HL); // V3 2dB 
+    digitalWrite(16,1-v4_HL); // V4 4dB
+    digitalWrite(27,1-v5_HL); // V5 8dB
+    digitalWrite(14,1-v6_HL); // V6 16dB
+  }
+  else if(pinArrange =="tp_p18-12_and_p26-p14" || pinArrange =="default"){ 
+    //for throughput or default-use
     digitalWrite(18,v1_HL); // V1 0.5dB
     digitalWrite(19,v2_HL); // V2 1dB
     digitalWrite(23,v3_HL); // V3 2dB 
@@ -178,24 +212,39 @@ void HandOver4dBstep(int sleepTime,String controlPin){
 
 void Throughput4dBstep(int sleepTime,String controlPin){
   //up down
-  setPin(controlPin,0,0,0,0,0,0); // 0,32dB 
+  setPin(controlPin,0,0,0,0,0,0); // 0
   delay(sleepTime);  
-  setPin(controlPin,0,0,0,1,0,0); //4,28dB
+  setPin(controlPin,0,0,0,1,0,0); //4
   delay(sleepTime);
-  setPin(controlPin,0,0,0,0,1,0); //8,24dB
+  setPin(controlPin,0,0,0,0,1,0); //8
   delay(sleepTime);
-  setPin(controlPin,0,0,0,1,1,0); //12,20dB
+  setPin(controlPin,0,0,0,1,1,0); //12
   delay(sleepTime);
-  setPin(controlPin,0,0,0,0,0,1); //16,16dB
+  setPin(controlPin,0,0,0,0,0,1); //16
   delay(sleepTime);
-  setPin(controlPin,0,0,0,1,0,1); //20,12dB 
+  setPin(controlPin,0,0,0,1,0,1); //20
   delay(sleepTime);
-  setPin(controlPin,0,0,0,0,1,1); //24,8dB  
+  setPin(controlPin,0,0,0,0,1,1); //24
   delay(sleepTime);
-  setPin(controlPin,0,0,0,1,1,1); //28,4dB
+  setPin(controlPin,0,0,0,1,1,1); //28
   delay(sleepTime);
-  setPin(controlPin,1,1,1,1,1,1); //32,0dB  
+  setPin(controlPin,1,1,1,1,1,1); //32 
   delay(sleepTime);
+}
+
+void setxdB(int att,String controlPin){
+  switch(att){
+    case 0: setPin(controlPin,0,0,0,0,0,0);break; //0
+    case 4: setPin(controlPin,0,0,0,1,0,0);break; //4
+    case 8: setPin(controlPin,0,0,0,0,1,0);break; //8
+    case 12: setPin(controlPin,0,0,0,1,1,0);break; //12
+    case 16: setPin(controlPin,0,0,0,0,0,1);break; //16
+    case 20: setPin(controlPin,0,0,0,1,0,1);break; //20
+    case 24: setPin(controlPin,0,0,0,0,1,1);break; //24
+    case 28: setPin(controlPin,0,0,0,1,1,1);break; //28
+    case 32: setPin(controlPin,1,1,1,1,1,1);break; //32
+    default: Serial.print("cannot set loss");
+  }
 }
   
 void printLocalTime(){
@@ -204,7 +253,16 @@ void printLocalTime(){
     Serial.println("Failed to obtain time");
     return;
   }
-  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+  Serial.print(&timeinfo, "%A, %B %d %Y %H:%M:%S,");
+}
+
+void udpLocalTime(){
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time udp");
+    return;
+  }
+  udp.print(&timeinfo, "%A, %B %d %Y %H:%M:%S,");
 }
   
 // Thanks to https://algorithm.joho.info/arduino/string-split-delimiter/
